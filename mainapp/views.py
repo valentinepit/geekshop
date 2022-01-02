9fe710a3dd9e718b063ad5f1bc12e1fba5f85854
trmcreates <admin@payperyou.com>
2022-01-02 23:49:23 +0500

    lesson_5 basket

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,19 +1,30 @@
+import random
+
 from django.shortcuts import render, get_object_or_404
 
+from basketapp.models import Basket
 from mainapp.models import Product, ProductCategory
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    return []
 
 
 def index(request):
     context = {
         "title": 'Главная',
-        'products': Product.objects.all()[:4]
+        'products': Product.objects.all()[:4],
+        'basket': get_basket(request.user)
     }
     return render(request, 'mainapp/index.html', context=context)
 
 
 def contact(request):
     context = {
-        "title": 'Контакты'
+        "title": 'Контакты',
+        'basket': get_basket(request.user)
     }
     return render(request, 'mainapp/contact.html', context=context)
 
@@ -34,14 +45,18 @@ def products(request, pk=None):
             'links_menu': links_menu,
             "title": "Продукты",
             "category": category_item,
-            'products': products_list
+            'products': products_list,
+            'basket': get_basket(request.user)
         }
         return render(request, 'mainapp/products_list.html', context=context)
+
+    hot_product = random.sample(list(Product.objects.all()), 1)[0]
+    same_products = Product.objects.all()[3:5]
     context = {
         'links_menu': links_menu,
-        "title": "Продукты"
+        "title": "Продукты",
+        'hot_product': hot_product,
+        'same_products': same_products,
+        'basket': get_basket(request.user)
     }
     return render(request, 'mainapp/products.html', context=context)
-
-
-
