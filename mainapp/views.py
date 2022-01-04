a331ae43aec75a1596d3e81188bca7ee2a8b3110
trmcreates <admin@payperyou.com>
2022-01-04 15:23:50 +0500

    lesson_5 basket

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,7 +9,16 @@ from mainapp.models import Product, ProductCategory
 def get_basket(user):
     if user.is_authenticated:
         return Basket.objects.filter(user=user)
-    return []
+    return None
+
+
+def get_hot_product():
+    return random.sample(list(Product.objects.all()), 1)[0]
+
+
+def get_same_products(hot_product):
+    products_list = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+    return products_list
 
 
 def index(request):
@@ -50,8 +59,8 @@ def products(request, pk=None):
         }
         return render(request, 'mainapp/products_list.html', context=context)
 
-    hot_product = random.sample(list(Product.objects.all()), 1)[0]
-    same_products = Product.objects.all()[3:5]
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
     context = {
         'links_menu': links_menu,
         "title": "Продукты",
@@ -60,3 +69,15 @@ def products(request, pk=None):
         'basket': get_basket(request.user)
     }
     return render(request, 'mainapp/products.html', context=context)
+
+
+def product(request, pk):
+    links_menu = ProductCategory.objects.all()
+    context = {
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+        'links_menu': links_menu
+
+    }
+
+    return render(request, 'mainapp/product.html', context)
