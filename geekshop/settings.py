259b1396b5d184b6dc64192250cdf8e4a711bb34
Valentine <42617437+valentinepit@users.noreply.github.com>
2022-01-04 18:15:31 +0500

    Revert "Lesson 5"

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
-    'basketapp',
     'authapp',
 ]
 
@@ -134,5 +133,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGIN_URL = '/auth/login'
